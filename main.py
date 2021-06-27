470f178b8f1c5e212c45a1ddfd59e07d48271652
Rodrigo Rios <rodrigo.rras@gmail.com>
2021-06-27 16:06:00 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,17 +6,49 @@ class JetFighter:
         self.maximum_speed = maximum_speed
         self.maximum_range = maximum_range
         self.combat_radius = combat_radius
-        self.roll_rate     = roll_rate
+        self.roll_rate = roll_rate
         self.flight_ceiling = flight_ceiling
         self.max_payload = max_payload
         self.number_engines = number_engines
 
 
+    def shuffle_cards(self):
+        pass
 
-F14_tomcat = JetFighter("38 million", 0.88,  "2485 km/h", "2960 Km", "960 Km", 720 , "15500m", "6700 Kg", 2 )
-#F16_falcon = JetFighter("64 million"),1.096, "2531,34",
 
+    def set_of_cards(self, List_of_Players):
+        # Return An Equal amount of cards per user player
+        pass
+
+
+    def card_comparison_result(self, JetFighter, choosen_parameter, List_of_players ):
+        # Return the winner of the Round
+
+        if not isinstance(self, JetFighter):
+            raise ValueError('the Top Trumps comparison requires a Top Trump card Object')
+            pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+F14_tomcat = JetFighter("38 million", 0.88, "2485 km/h", "2960 Km", "960 Km", 720, "15500m", "6700 Kg", 2)
+# F16_falcon = JetFighter("64 million"),1.096, "2531,34",
+
+
+JetFighter.__repr__()
 
 if __name__ == '__main__':
-    print("F14 Speed = " , F14_tomcat.maximum_speed)
+    print("F14 Speed = ", F14_tomcat.maximum_speed)
 
